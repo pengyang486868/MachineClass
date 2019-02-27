70d3862d7cbfe953cb66ece16e87dee52f9d2cb3
pengyang <pengyang@PENGY>
2019-02-27 23:31:00 +0800

    std stat

--- LessonMachineLearn.GradientDescent/LessonMachineLearn.GradientDescent/LessonMachineLearn.GradientDescent.py
+++ LessonMachineLearn.GradientDescent/LessonMachineLearn.GradientDescent/LessonMachineLearn.GradientDescent.py
@@ -2,6 +2,19 @@ import csv
 import numpy as np
 import pandas as pd
 
+def stat(a):
+    sum = 0
+    count = 0
+    for da in a:
+        sum = sum + da
+        count = count + 1
+    aver = sum / count
+    sqsum = 0
+    for da in a:
+        sqsum = sqsum + (da - aver) ** 2
+    sdev = (sqsum / count) ** 0.5
+    return aver,sdev
+
 filename = 'housing.csv'
 df = pd.DataFrame(columns = ["area", "room", "price"])
 with open(filename) as f:
@@ -10,8 +23,7 @@ with open(filename) as f:
         d = np.array(row,dtype=np.float)
         df.loc[df.shape[0] + 1] = d
 #print(df)
-col=df['area']
-sum=0
-for da in col:
-    sum=sum+da
+area = df['area']
+room = df['room']
+av,sd = stat(area)
 print()
