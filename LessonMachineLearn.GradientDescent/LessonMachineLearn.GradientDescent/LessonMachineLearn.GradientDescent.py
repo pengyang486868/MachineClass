35ee562f20f850f446f8482ee11702e16eaad11a
pengyang <pengyang@PENGY>
2019-02-27 23:16:42 +0800

    3

--- LessonMachineLearn.GradientDescent/LessonMachineLearn.GradientDescent/LessonMachineLearn.GradientDescent.py
+++ LessonMachineLearn.GradientDescent/LessonMachineLearn.GradientDescent/LessonMachineLearn.GradientDescent.py
@@ -1,8 +1,17 @@
 import csv
+import numpy as np
+import pandas as pd
 
 filename = 'housing.csv'
+df = pd.DataFrame(columns = ["area", "room", "price"])
 with open(filename) as f:
     reader = csv.reader(f)
     for row in reader:
-        for num in row:
-            print(int(num))
+        d = np.array(row,dtype=np.float)
+        df.loc[df.shape[0] + 1] = d
+#print(df)
+col=df['area']
+sum=0
+for da in col:
+    sum=sum+da
+print()
